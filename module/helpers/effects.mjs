38e7643a293b77812a65866a769722d6cd780413
pazindorb <56399197+pazindorb@users.noreply.github.com>
2023-08-25 14:07:51 +0200

    Updating system to 11.308

--- module/helpers/effects.mjs
+++ module/helpers/effects.mjs
@@ -20,7 +20,7 @@ export function prepareActiveEffectCategories(effects) {
 
     // Iterate over active effects, classifying them into categories
     for ( let effect of effects ) {
-      effect._getSourceName(); // Trigger a lookup for the source name
+      effect.orignName = effect.sourceName;
       if ( effect.disabled ) categories.inactive.effects.push(effect);
       else if ( effect.isTemporary ) categories.temporary.effects.push(effect);
       else categories.passive.effects.push(effect);
