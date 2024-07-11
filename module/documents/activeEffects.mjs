ff9b17a26042f3681a2e033105edf2debcb71a1d
pazindorb <xxpazindorxx@gmail.com>
2024-07-11 10:18:10 +0200

    Active effects missing icons on v11 version cheat fix

--- module/documents/activeEffects.mjs
+++ module/documents/activeEffects.mjs
@@ -4,6 +4,21 @@
 export default class DC20RpgActiveEffect extends ActiveEffect {
 
   /**@override */
+  prepareDerivedData() {
+    // v11 compatibility (TODO: REMOVE LATER)
+    if (!this.img && !this.icon) {
+      if (this.parent.constructor.name === "DC20RpgActor") {
+        const itemId = this.origin.split("Item.")[1];
+        const item = this.parent.items.get(itemId);
+        if (item) this.icon = item.img;
+        else this.icon = this.parent.img; 
+      }
+      else this.icon = this.parent.img; 
+    }
+    // =====================
+  }
+
+  /**@override */
   _applyUpgrade(actor, change, current, delta, changes) {
     // There is a bug where if update doesn't overrides change value it causes it to become undefined
     super._applyUpgrade(actor, change, current, delta, changes);
