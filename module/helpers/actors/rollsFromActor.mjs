5c99127cc86e6a936a4c428c4c6f9ae94b6010ba
pazindorb <xxpazindorxx@gmail.com>
2024-07-11 09:32:40 +0200

    Reverting some compatibility changes

--- module/helpers/actors/rollsFromActor.mjs
+++ module/helpers/actors/rollsFromActor.mjs
@@ -552,7 +552,7 @@ function _prepareEffectsFromItems(item) {
   const effects = [];
   item.effects.forEach(effect => {
     effects.push({
-      img: effect.img || effect.icon,  // v11 compatibility (TODO: REMOVE LATER)
+      img: effect.img,
       name: effect.name,
       uuid: effect.uuid,
     })
