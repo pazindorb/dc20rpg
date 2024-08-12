643c052f88b988a4fcbaaefba246a2ed6deb0f2c
pazindorb <xxpazindorxx@gmail.com>
2024-08-12 12:47:55 +0200

    Small Fix

--- module/helpers/context-menu.mjs
+++ module/helpers/context-menu.mjs
@@ -4,11 +4,13 @@ export function closeContextMenu(html) {
 }
 
 export function itemContextMenu(item, event, html) {
+
   // Prepare content
   let content = '';
   content += `<a class="elem item-edit" data-item-id="${item._id}"><i class="fas fa-edit"></i><span>${game.i18n.localize('dc20rpg.sheet.items.editItem')}</span></a>`;
   content += `<a class="elem item-delete" data-item-id="${item._id}"><i class="fas fa-trash"></i><span>${game.i18n.localize('dc20rpg.sheet.items.deleteItem')}</span></a>`;
-  content += `<a class="elem item-copy" data-item-id="${item._id}"><i class="fas fa-copy"></i><span>${game.i18n.localize('dc20rpg.sheet.items.copyItem')}</span></a>`;
+  if (!["ancestry", "class", "subclass", "background"].includes(item.type)) 
+    content += `<a class="elem item-copy" data-item-id="${item._id}"><i class="fas fa-copy"></i><span>${game.i18n.localize('dc20rpg.sheet.items.copyItem')}</span></a>`;
   _showContextMenu(content, event, html, item);
 }
 
