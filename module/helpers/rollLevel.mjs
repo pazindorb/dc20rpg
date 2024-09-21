c9ca5415c68e937d694e955c11537d5234f02a68
pazindorb <xxpazindorxx@gmail.com>
2024-09-21 12:07:42 +0200

    Roll Level Bug Fix

--- module/helpers/rollLevel.mjs
+++ module/helpers/rollLevel.mjs
@@ -310,6 +310,8 @@ function _getSavePath(saveKey, actor) {
     const chaSave = actor.system.attributes.cha.save;
     saveKey = intSave >= chaSave ? "int" : "cha";
   }
+
+  if (attrKey === "prime") attrKey = actor.system.details.primeAttrKey;
   return `saves.${saveKey}`;
 }
 
