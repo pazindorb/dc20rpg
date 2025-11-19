9598de8d89e92d5f7b8a1b51d8a362dce702faf6
pazindorb <xxpazindorxx@gmail.com>
2025-11-19 09:54:19 +0100

    Preparations for 0.9.8.2

--- module/dc20rpg.mjs
+++ module/dc20rpg.mjs
@@ -125,8 +125,8 @@ Hooks.once('init', async function() {
 /* -------------------------------------------- */
 Hooks.once("ready", async function() {
   // await runMigrationCheck();
-  // await testMigration("0.9.8.0", "0.9.8.1", new Set(["dc20-core-rulebook", "dc20-magic-pack"]));
-  // await testMigration("0.9.8.0", "0.9.8.1");
+  // await testMigration("0.9.8.1", "0.9.8.2", new Set(["dc20-core-rulebook", "dc20-magic-pack"]));
+  // await testMigration("0.9.8.1", "0.9.8.2");
 
   /* -------------------------------------------- */
   /*  Hotbar Macros                               */
